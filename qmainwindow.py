3d11357c95f353666c4057b482e78360bbc34a3a
rubentri <rubentriardians@gmail.com>
2024-10-18 00:29:08 +0800

    PyQt5 Visual 3

---
+++ qmainwindow.py
@@ -0,0 +1,13 @@
+# Menggunakan QMainWindow
+from PyQt5.QtWidgets import QApplication, QPushButton, QLabel, QMainWindow
+
+app = QApplication([])
+window = QMainWindow()
+label = QLabel(window)
+label.setText("Label1")
+label.move(200,0 )
+button = QPushButton(window)
+button.setText("Button1")
+
+window.show()
+app.exec_()
